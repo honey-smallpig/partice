7983818561272ec36e2690135e900801dbc64942
youlanda <youlanda_perry@163.com>
2021-09-20 16:20:51 +0800

    fix 下载数据

--- Demo/src/crowller.ts
+++ Demo/src/crowller.ts
@@ -2,7 +2,19 @@
 
 import superagent from "superagent";
 import cheerio from 'cheerio';
+import fs from 'fs';
+import path from 'path';
 
+interface Cource {
+    title: string
+}
+interface Source {
+    time: number,
+    data: Cource[]
+}
+interface Filesource {
+    [propName: number]: Cource[];
+}
 class Crowller {
     private keyVale = 'secretKey';
     private url = `http://www.dell-lee.com/`;
@@ -15,12 +27,30 @@ class Crowller {
     getArtice(html:string) {
         const $ = cheerio.load(html);
         const source = $('.course-item');
+        const textAry:Cource[] = [];
         source.map((index,element) => {
-            const getItem = $(element).find('.course-desc').text();
-            console.log(getItem)
+            const getItem = $(element).find('.course-desc');
+            const title =  getItem.eq(0).text();
+            textAry.push({title})         
         })
+        const result = {
+            time: (new Date()).getTime(),
+            data: textAry
+        }
+        console.log(result)
+        this.getJsonData(result);
     }
     
+    //把拿到的数据存到文件里面
+    getJsonData(source:Source) {
+        const filePath = path.resolve(__dirname,'../data/cource.json');
+        let fileCount:Filesource = {};
+        if(fs.existsSync(filePath)) {
+            fileCount = JSON.parse(fs.readFileSync(filePath,'utf-8'));
+        }
+        fileCount[source.time] = source.data;
+        fs.writeFileSync(filePath,JSON.stringify(fileCount))
+    }
 
     constructor(){
         this.getHtml();
